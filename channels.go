6f87d0ea940761f954eb64aadcdf0f050eb38a83
a-know <a.know.3373@gmail.com>
2020-03-29 13:40:45 +0900

    Implements; POST channels

--- channels.go
+++ channels.go
@@ -1,20 +1,37 @@
 package pi
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
 type channelsCommand struct {
-	Get getChannelsCommand `description:"get Channel Definitions" command:"get" subcommands-optional:"true"`
+	Create createChannelCommand `description:"create Channel" command:"create" subcommands-optional:"true"`
+	Get    getChannelsCommand   `description:"get Channel Definitions" command:"get" subcommands-optional:"true"`
+}
+
+type createChannelCommand struct {
+	Username string `short:"u" long:"username" description:"User name of channel owner."`
+	ID       string `short:"i" long:"channel-id" description:"ID for identifying the channel." required:"true"`
+	Name     string `short:"n" long:"name" description:"The name of the channel." required:"true"`
+	Type     string `short:"t" long:"type" description:"The type for notification." required:"true"`
+	Detail   string `short:"d" long:"detail" description:"Object that specifies the details of the type. It is specified as JSON string." required:"true"`
+}
+
+type createChannelParam struct {
+	ID     string          `json:"id"`
+	Name   string          `json:"name"`
+	Type   string          `json:"type"`
+	Detail json.RawMessage `json:"detail"`
 }
 
 type getChannelsCommand struct {
 	Username string `short:"u" long:"username" description:"User name of channel owner."`
 }
 
-func (gG *getChannelsCommand) Execute(args []string) error {
-	req, err := generateGetChannelsRequest(gG)
+func (cC *createChannelCommand) Execute(args []string) error {
+	req, err := generateCreateChannelRequest(cC)
 	if err != nil {
 		return err
 	}
@@ -23,8 +40,43 @@ func (gG *getChannelsCommand) Execute(args []string) error {
 	return err
 }
 
-func generateGetChannelsRequest(gG *getChannelsCommand) (*http.Request, error) {
-	username, err := getUsername(gG.Username)
+func generateCreateChannelRequest(cC *createChannelCommand) (*http.Request, error) {
+	username, err := getUsername(cC.Username)
+	if err != nil {
+		return nil, err
+	}
+
+	paramStruct := &createChannelParam{
+		ID:     cC.ID,
+		Name:   cC.Name,
+		Type:   cC.Type,
+		Detail: json.RawMessage(cC.Detail),
+	}
+
+	req, err := generateRequestWithToken(
+		"POST",
+		fmt.Sprintf("v1/users/%s/channels", username),
+		paramStruct,
+	)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to generate create api request : %s", err)
+	}
+
+	return req, nil
+}
+
+func (gC *getChannelsCommand) Execute(args []string) error {
+	req, err := generateGetChannelsRequest(gC)
+	if err != nil {
+		return err
+	}
+
+	err = doRequest(req)
+	return err
+}
+
+func generateGetChannelsRequest(gC *getChannelsCommand) (*http.Request, error) {
+	username, err := getUsername(gC.Username)
 	if err != nil {
 		return nil, err
 	}
--- channels_test.go
+++ channels_test.go
@@ -12,6 +12,26 @@ var channelTests = []struct {
 	exitCode int
 }{
 	{
+		name:     "create channel - not specify id",
+		input:    []string{"channels", "create", "--name", "test-name", "--type", "slack", "--detail", `{"url":"https://hooks.slack.com/services/T035DA4QD/B06LMAV40/xxxx","userName":"Pixela Notification","channelName":"pixela-notify"}`},
+		exitCode: 1,
+	},
+	{
+		name:     "create channel - not specify name",
+		input:    []string{"channels", "create", "--channel-id", "test-id", "--type", "slack", "--detail", `{"url":"https://hooks.slack.com/services/T035DA4QD/B06LMAV40/xxxx","userName":"Pixela Notification","channelName":"pixela-notify"}`},
+		exitCode: 1,
+	},
+	{
+		name:     "create channel - not specify type",
+		input:    []string{"channels", "create", "--channel-id", "test-id", "--name", "test-name", "--detail", `{"url":"https://hooks.slack.com/services/T035DA4QD/B06LMAV40/xxxx","userName":"Pixela Notification","channelName":"pixela-notify"}`},
+		exitCode: 1,
+	},
+	{
+		name:     "create channel - not specify detail",
+		input:    []string{"channels", "create", "--channel-id", "test-id", "--name", "test-name", "--type", "slack"},
+		exitCode: 1,
+	},
+	{
 		name:     "get channel definition - not specify username",
 		input:    []string{"channels", "get"},
 		exitCode: 1,
@@ -27,6 +47,49 @@ func TestChannel(t *testing.T) {
 		if exitCode != tt.exitCode {
 			t.Errorf("%s(exitCode): out=%d want=%d", tt.name, exitCode, tt.exitCode)
 		}
+	}
+}
+
+func TestGenerateCreateChannelRequest(t *testing.T) {
+	// prepare
+	beforeAPIBaseEnv, beforeTokenEnv, afterAPIBaseEnv, _ := prepare()
+
+	testUsername := "c-know"
+	testID := "test-id"
+	testName := "test-name"
+	testType := "slack"
+	testDetail := `{"url":"https://hooks.slack.com/services/T035DA4QD/B06LMAV40/xxxx","userName":"Pixela Notification","channelName":"pixela-notify"}`
+	cmd := &createChannelCommand{
+		Username: testUsername,
+		ID:       testID,
+		Name:     testName,
+		Type:     testType,
+		Detail:   testDetail,
+	}
+
+	// run
+	req, err := generateCreateChannelRequest(cmd)
+
+	// cleanup
+	cleanup(beforeAPIBaseEnv, beforeTokenEnv)
+
+	// assertion
+	if err != nil {
+		t.Errorf("Unexpected error occurs. %s", err)
+	}
+	if req.Method != "POST" {
+		t.Errorf("Unexpected request method. %s", req.Method)
+	}
+	if req.URL.String() != fmt.Sprintf("https://%s/v1/users/%s/channels", afterAPIBaseEnv, testUsername) {
+		t.Errorf("Unexpected request path. %s", req.URL.String())
+	}
+	b, err := ioutil.ReadAll(req.Body)
+	defer req.Body.Close()
+	if err != nil {
+		t.Errorf("Failed to read request body. %s", err)
+	}
+	if string(b) != fmt.Sprintf(`{"id":"%s","name":"%s","type":"%s","detail":%s}`, testID, testName, testType, testDetail) {
+		t.Errorf("Unexpected request body. %s", string(b))
 	}
 }
 
